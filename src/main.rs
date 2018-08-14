148a79c12e7bc6c594282d6c6de9d86f661ffa3e
ingared8 <gangareddy.ts@gmail.com>
2018-08-13 19:39:02 -0700

    Referencing by adress example added

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,20 @@ fn color_write() {
 
 fn string_test() {
     let s = String::from("Shree");
-     heap_example(s);
+     //heap_example(s);
+    heap_example_borrow(&s);
 }
 
 
 fn heap_example(input: String) {
     let mystr =  input;
     //let _otherstr = mystr;
+    println!("{}", mystr);
+}
+
+fn heap_example_borrow(input: &String) {
+    let mystr =  input;
+    let _otherstr = mystr;
     println!("{}", mystr);
 }
 
@@ -43,3 +50,5 @@ fn stack_example(input: i32){
     let _y = x;
     println!("{}", x);
 }
+
+
